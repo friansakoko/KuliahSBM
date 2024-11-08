81f93da940f34dfaaf4b370e43c99ccfbe23fb6a
Koko Friansa <koko.friansa@tse.itera.ac.id>
2024-11-08 08:41:13 +0700

    Update Matrix.R
    Terdapat pembaruan keterangan pada baris pertama

--- Matrix.R
+++ Matrix.R
@@ -1,4 +1,7 @@
 # Create two 2x3 matrices.
+# Terdapat kodingan baru
+# Yaitu perhitungan rata-rata
+
 matrix1 <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3)
 matrix2 <- matrix(c(7, 8, 9, 10, 11, 12), nrow = 3)
 
